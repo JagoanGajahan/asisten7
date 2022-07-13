81da50e5a99d63e66a6728a821d8ba6fe0c9d1c6
jagoaneasy <99625712+jagoaneasy@users.noreply.github.com>
2022-07-13 15:49:51 +0700

    Update mainan.py

--- mainan.py
+++ mainan.py
@@ -1,6 +1,6 @@
 from pyrogram import Client,idle
 import datetime,os
-from databasefile import st1
+#from databasefile import st1
 st1= os.environ.get("st1")
 asisten1=Client("nks",session_string=st1,plugins=dict(root="main1"))
 asisten1.start()
