2f4a4e55363b30e12270e684423d970e68184305
jagoaneasy <99625712+jagoaneasy@users.noreply.github.com>
2022-07-13 22:00:49 +0700

    Update mainan.py

--- mainan.py
+++ mainan.py
@@ -2,7 +2,15 @@ from pyrogram import Client,idle
 import datetime,os
 #from databasefile import st1
 st1= os.environ.get("st1")
+st2= os.environ.get("st2")
+sb1= os.environ.get("sb1")
 asisten1=Client("nks",session_string=st1,plugins=dict(root="main1"))
 asisten1.start()
+asisten2=Client("vnl",session_string=st2,plugins=dict(root="main2"))
+asisten2.start()
+asisten3=Client("cwm",session_string=sb1,plugins=dict(root="main3"))
+asisten3.start()
 idle()
 asisten1.stop()
+asisten2.stop()
+asisten3.stop()
