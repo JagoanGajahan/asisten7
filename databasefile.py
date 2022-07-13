a51aca7e521d8b1392cca3bc96623925176a2c19
jagoaneasy <99625712+jagoaneasy@users.noreply.github.com>
2022-07-13 16:22:53 +0700

    Update databasefile.py

--- databasefile.py
+++ databasefile.py
@@ -1,12 +1,12 @@
 import os
 
-laporan = os.environ.get("laporan")
-kgb = os.environ.get("laporan")
-mialc = os.environ.get("mialc")
-rangkumanch = os.environ.get("rangkumanch")
-chnokos = os.environ.get("chnokos")
-iklanch = os.environ.get("iklanch")
-chwajib1 = os.environ.get("chwajib1")
-grwajib1 = os.environ.get("grwajib1", "0")
+laporan = int(os.environ.get("laporan"))
+kgb = int(os.environ.get("laporan"))
+mialc = int(os.environ.get("mialc"))
+rangkumanch = int(os.environ.get("rangkumanch"))
+chnokos = int(os.environ.get("chnokos"))
+iklanch = int(os.environ.get("iklanch"))
+chwajib1 = int(os.environ.get("chwajib1"))
+grwajib1 = int(os.environ.get("grwajib1", "0"))
 urutan = os.environ.get("urutan")
 
