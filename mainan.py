582bedb39014f253082a7af0f88a8595a18d6b2b
jagoaneasy <99625712+jagoaneasy@users.noreply.github.com>
2022-07-13 15:48:15 +0700

    Create mainan.py

---
+++ mainan.py
@@ -0,0 +1,8 @@
+from pyrogram import Client,idle
+import datetime
+from databasefile import st1
+
+asisten1=Client("nks",session_string=st1,plugins=dict(root="main1"))
+asisten1.start()
+idle()
+asisten1.stop()
