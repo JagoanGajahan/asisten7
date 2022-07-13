676ff556dfdd14fea1e476b8ec3928626c9ae7e7
jagoaneasy <99625712+jagoaneasy@users.noreply.github.com>
2022-07-13 14:42:04 +0700

    Create databasefile.py

---
+++ databasefile.py
@@ -0,0 +1,12 @@
+import os
+
+laporan = os.environ.get("laporan")
+kgb = os.environ.get("laporan")
+mialc = os.environ.get("mialc")
+rangkumanch = os.environ.get("rangkumanch")
+chnokos = os.environ.get("chnokos")
+iklanch = os.environ.get("iklanch")
+chwajib1 = os.environ.get("chwajib1")
+grwajib1 = os.environ.get("grwajib1", "0")
+urutan = os.environ.get("urutan")
+
