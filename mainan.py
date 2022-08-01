e7f14a607fc888639139b615bf40b781f41a1643
jagoaneasy <99625712+jagoaneasy@users.noreply.github.com>
2022-08-01 10:28:47 +0700

    Update mainan.py

--- mainan.py
+++ mainan.py
@@ -4,6 +4,7 @@ import datetime,os
 st1= os.environ.get("st1")
 st2= os.environ.get("st2")
 sb1= os.environ.get("sb1")
+kb1= os.environ.get("kb1")
 laporan = int(os.environ.get("laporan", ""))
 def saiki():
     s=datetime.datetime.now()
@@ -17,7 +18,8 @@ asisten2=Client("vnl",session_string=st2,plugins=dict(root="main2"))
 asisten2.start()
 asisten3=Client("cwm",session_string=sb1,plugins=dict(root="main3"))
 asisten3.start()
-
+asisten4=Client("kmk",session_string=kb1,plugins=dict(root="main4"))
+asisten.start()
 sa=saiki()
 asisten1.send_message(laporan,"check system from dyno",schedule_date=sa)
 
@@ -25,3 +27,4 @@ idle()
 asisten1.stop()
 asisten2.stop()
 asisten3.stop()
+asisten4.stop()
