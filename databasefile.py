72d9c2a4ba6509ba1776c3eaddedaaca225fd32b
jagoaneasy <99625712+jagoaneasy@users.noreply.github.com>
2022-08-01 10:30:51 +0700

    Update databasefile.py

--- databasefile.py
+++ databasefile.py
@@ -10,3 +10,7 @@ chwajib1 = int(os.environ.get("chwajib1"))
 grwajib1 = int(os.environ.get("grwajib1", "0"))
 urutan = os.environ.get("urutan")
 
+komikbot=os.environ.get("komikbot")
+tampukomik=int(os.environ.get("tampukomik"))
+urutan2 = os.environ.get("urutan2")
+dikodenya=os.environ.get("dikodenya")
