51021a5cdff6ce160d920e35f8b128875fe1c0fe
jagoaneasy <99625712+jagoaneasy@users.noreply.github.com>
2022-08-31 10:55:57 +0700

    Update b4.py

--- main3/b4.py
+++ main3/b4.py
@@ -164,8 +164,8 @@ async def tes1(c,p):
                 while p.id in tangkapkiriman:
                     if p.id==tangkapkiriman[0]:
                         for sipenerima in sipenerimakiriman:
-                            print("penerima",sipenerima)
-                            await p.reply_to_message.copy(sipenerima)
+                            #print("penerima",sipenerima)
+                            await p.copy(sipenerima)
                             await asyncio.sleep(0.2)
                         await asyncio.sleep(3)
                         tangkapankiriman.pop(0)
